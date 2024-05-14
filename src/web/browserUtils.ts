e5a0a58a96be522030cf0ad70027a2dfa5df6f0a
Charlie Murphy <charliemurphyj@gmail.com>
2024-05-13 20:51:49 -0400

    Update browserUtils.ts

--- src/web/browserUtils.ts
+++ src/web/browserUtils.ts
@@ -1,4 +1,5 @@
 const isFirefox = navigator.userAgent.toLowerCase().includes('firefox');
 const isChromium = 'chrome' in window;
+const isChromium = typeof window !== 'undefined' ? 'chrome' in window : false;
 
 export {isFirefox, isChromium};
